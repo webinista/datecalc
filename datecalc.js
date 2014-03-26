b55352dcc7130b256586d5973bd45f88221e936b
tiffany <tiff@webinista.local>
2014-03-26 16:38:01 -0700

    Almost completed date calculations.

--- datecalc.js
+++ datecalc.js
@@ -1,14 +1,18 @@
-var start, result, difference, Units = {};
+var start, result, difference, datecalc, Units = {};
 
 start  = document.getElementById('startdate');
 result = document.getElementById('result');
 difference = document.getElementById('difference');
+datecalc = document.getElementById('datecalc');
 
 var year, month, day, hour, minute, second, millisecond;
 
-// new Date(year, month, day, hour, minute, second, millisecond);
+/* 
+TODO:
+Figure out how to make weeks happen
+*/
 
-start.valueAsNumber;
+// var output = new Date(Units.year, Units.month, Units.day, Units.hour, Units.minute, Units.second, Units.millisecond);
 
 function zeroPadLeft(input, length){
     var zero = '0', pad = '', len, padded, inp, extract;
@@ -29,7 +33,7 @@ function zeroPadLeft(input, length){
     return padded.substr(extract);
 }
 
-function setToday(updateField){
+function setToday (updateField) {
     if( updateField.type == 'text'){
         var d, now = new Date(), date = [];
         
@@ -39,10 +43,69 @@ function setToday(updateField){
         
         d = date.join('/');
         updateField.value = d;
-    } else {
-        
+    } else {    
         updateField.valueAsNumber = new Date();
     }
 }
 
-setToday(startdate);
+function parseDateUnit (value) {
+   return value.split(' ')[1];
+}
+
+function parseNumber (value) {
+   return parseInt(value, 10);
+}
+
+function calculateDate (inputDate, difference) {
+    var input, diff, num, unit, wks, d;
+    
+    input = new Date(inputDate);
+    
+    num  = parseNumber(difference);
+    unit = parseDateUnit(difference);
+    
+    
+    Units.years     =  input.getFullYear();
+    Units.months    =  input.getUTCMonth();
+    Units.month     =  input.getUTCMonth() + 1;
+    Units.weeks     =  (60 * 60 * 24 * 7)
+    Units.days      =  input.getUTCDate();
+    Units.hours     =  input.getUTCHours();
+    Units.minutes   =  input.getUTCMinutes();
+    Units.seconds   =  input.getUTCSeconds();
+    Units.milliseconds  =  input.getUTCMilliseconds();
+    
+    if(Units[unit]){
+        if ( /week/.test(unit) ) {
+            
+            /* Multiply week "constant" by number of them */
+            wks = Units.weeks * num;
+            
+            Units.seconds = Units.seconds + wks;
+       
+        } else {
+            Units[unit] = Units[unit] + num; 
+        }
+         
+    } else {
+        throw new TypeError('Units must be year(s), month(s), week(s), day(s), hour(s), minute(s), or second(s), for example: -3 weeks.');  
+    }
+    
+    d = new Date(Units.years, Units.months, Units.days, Units.hours, Units.minutes, Units.seconds, Units.milliseconds);
+    return d;
+  }
+
+datecalc.addEventListener('submit', function(e){
+    e.preventDefault();
+    
+    var today = !!startdate.valueAsNumber ? startdate.valueAsNumber : startdate.value;
+        
+    console.log( calculateDate(today, difference.value) );
+});
+
+window.addEventListener('DOMContentLoaded', function(e){
+    setToday(startdate);
+});
+
+
+
